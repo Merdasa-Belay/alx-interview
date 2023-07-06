c0652c9c85f4c91a14066a87cbe67303045de47e
Merdasa-Belay <merdasabelay@gmail.com>
2023-07-07 02:28:51 +0300

    0x01. Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
-"""lockboxes"""
+"""
+Method that determines if all the boxes can be opened.
+"""
 
 
 def canUnlockAll(boxes):
-
-    if not boxes:
-        return False
-    unlocked = []
-    i = 0
-    length = len(boxes)
-
-    for i, box in enumerate(boxes):
-        if not box:
-            continue
-        for key in box:
-            if key < length and key not in unlocked and key != i:
-                unlocked.append(key)
-    if len(unlocked) == length:
-        return True
-    return False
+    num_of_boxes = len(boxes)
+    list_of_keys = boxes[0]
+    box_locked = [False] + [True] * (num_of_boxes - 1)
+    for key in list_of_keys:
+        if ((key < num_of_boxes) and (box_locked[key] is True)):
+            box_locked[key] = False
+            list_of_keys.extend(boxes[key])
+    return not any(box_locked)
